990e4817dc4303b5bc26cf8ab57511d3eeab4f7c
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-09-25 18:15:23 -0500

    feature: Añade validacion al formulario

--- js/app.js
+++ js/app.js
@@ -8,16 +8,44 @@ class UI {
         presupuesto.textContent = valor;
         restante.textContent = valor;
     }
+
+    mostrarMensaje(type, message) {
+        this.type = type;
+        this.message = message;
+
+        // agrega clase y mensaje al elemento creado
+        const alert = document.createElement('p');
+        alert.textContent = message;
+        alert.classList.add('message');
+
+
+        if(this.type == 'error') {
+            alert.classList.add('error')
+        } else {
+            alert.classList.add('sucess')
+        }
+
+        //agrega el mensaje al html 
+        const container = document.querySelector('.container:first-child');
+        container.insertBefore(alert, form);
+
+        
+        setTimeout(() => {
+            alert.remove();
+        }, 3000)
+    }
 }
 
 //ASIGNAR INSTANCIAS
-
 const interface = new UI();
 
 loadEvents()
 function loadEvents() {
+    const form = document.querySelector('#form');
 
     document.addEventListener('DOMContentLoaded', obtenerCosto);
+    form.addEventListener('submit', validarFormulario);
+
 
     // obtiene el presupuesto inicial
     function obtenerCosto() {
@@ -31,5 +59,32 @@ function loadEvents() {
         // agrega valor al html
         interface.mostrarCosto(valor);
     }
+
+    // valida campos del formulario
+    function validarFormulario(event) {
+        event.preventDefault();
+
+        const gastos = form.querySelector('#gasto');
+        const cantidad = form.querySelector('#cantidad');
+
+        // verifica y elimina alerta existente
+        const exist = document.querySelector('.message');
+        if(exist != null) {
+            exist.remove();
+        }
+
+        // muestra mensaje de error
+        if(gastos.value.trim() === '' || cantidad.value === '') {
+            interface.mostrarMensaje('error', 'Ambos datos son obligatorios');
+            return;
+        } else if (cantidad.value <= 0) {
+            interface.mostrarMensaje('error', 'La cantidad ingresada no es válida, intente de nuevo por favor');
+            return;
+        }
+
+        // muestra mensaje de éxito
+        interface.mostrarMensaje('sucess', 'Gasto agregado a la lista.')
+    }
+
 
 }
