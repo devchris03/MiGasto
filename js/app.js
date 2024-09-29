43f21c7a1121997063f8786065224fae8e95959d
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-09-28 19:45:00 -0500

    feature: Agrega lista de gastos en el html y calcula restante

--- js/app.js
+++ js/app.js
@@ -1,89 +1,149 @@
-// CONSTRUCTOR
-class UI {
+// -------------------- VARIABLES --------------------
+const form = document.querySelector('#form');
 
-    mostrarCosto(valor) {
-        const presupuesto = document.querySelector('#presupuestoText');
-        const restante = document.querySelector('#restanteText');
-        
-        presupuesto.textContent = valor;
-        restante.textContent = valor;
+// -------------------- EVENTOS --------------------
+document.addEventListener('DOMContentLoaded', getBudget);
+form.addEventListener('submit', expenses)
+
+// -------------------- CLASSES --------------------
+class Data {
+    constructor(cost) {
+        this.budget = Number(cost);
+        this.remainder = Number(cost);
+        this.expensesList = [];
     }
 
-    mostrarMensaje(type, message) {
-        this.type = type;
-        this.message = message;
+    getdata(expense, quantity) {
+        this.expense = expense;
+        this.quantity = Number(quantity);
 
-        // agrega clase y mensaje al elemento creado
+        const Obj = {
+            gasto: this.expense,
+            cantidad: this.quantity,
+        }
+
+        this.expensesList = [...this.expensesList, Obj];
+    }
+
+    restar(list) {
+        list.forEach(item => {
+            return this.remainder -= item.cantidad;
+        })
+    }
+}
+
+
+class UI {
+
+    showBudget(cost) {
+        const presupuestoText = document.querySelector('#presupuestoText');
+        const restanteText = document.querySelector('#restanteText');
+    
+        restanteText.textContent = cost.remainder; 
+        presupuestoText.textContent = cost.budget; 
+    }
+
+    showAlert(type, text) {
+        this.type = type;
+        this.text  = text;
+
         const alert = document.createElement('p');
-        alert.textContent = message;
+        alert.textContent = this.text;
         alert.classList.add('message');
 
-
-        if(this.type == 'error') {
-            alert.classList.add('error')
+        if(this.type === 'error') {
+            alert.classList.add('error');
         } else {
-            alert.classList.add('sucess')
+            alert.classList.add('sucess');
         }
 
-
-        const container = document.querySelector('.container:first-child');
+        const container = document.querySelector('.container');
         
         // verifica y elimina alerta existente
-        const exist = document.querySelector('.message');
+        const exist = container.querySelector('.message');
         if(exist) {
-            exist.remove();
+            exist.remove()
         }
         
-        //agrega el mensaje al html 
+        // inserta alerta
         container.insertBefore(alert, form);
-        
+
         setTimeout(() => {
             alert.remove();
         }, 3000)
     }
+
+    showList (list) {
+        list.forEach(item => {
+            const {cantidad, gasto} = item;
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <span>${gasto}</span>
+                <span>${cantidad}</span>
+            `;
+
+            const listContainer = document.querySelector('#listContainer');
+
+            listContainer.appendChild(listItem);
+        })
+    }
+
+    showRemain(result) {
+        document.querySelector('#restanteText').textContent = result;
+    }
+
 }
 
-//ASIGNAR INSTANCIAS
+// -------------------- ASIGNAR --------------------
+let data;
 const interface = new UI();
+// -------------------- FUNCIONES --------------------
 
-loadEvents()
-function loadEvents() {
-    const form = document.querySelector('#form');
+// obtiene presupuesto
+function getBudget() {
+    const cost = prompt('¿Cúal es tu presupuesto?');
 
-    document.addEventListener('DOMContentLoaded', obtenerCosto);
-    form.addEventListener('submit', validarFormulario);
-
-
-    // obtiene el presupuesto inicial
-    function obtenerCosto() {
-        const valor = prompt('¿Cúal es tu presupuesto?');
-
-        // vuelve a preguntar si en caso el valor no es el esperado
-        if(valor === '' || isNaN(valor) || valor === null || valor <= 0) {
-            window.location.reload();
-        }
-
-        // agrega valor al html
-        interface.mostrarCosto(valor);
+    if(cost === '' || isNaN(cost) || cost === null || cost <= 0) {
+        window.location.reload();
     }
 
-    // valida campos del formulario
-    function validarFormulario(event) {
-        event.preventDefault();
+    data = new Data(cost);
 
-        const gastos = form.querySelector('#gasto');
-        const cantidad = form.querySelector('#cantidad');
+    // mostrar presupuesto y restante
+    interface.showBudget(data);
+}
 
-        // muestra mensaje de error
-        if(gastos.value.trim() === '' || cantidad.value === '') {
-            interface.mostrarMensaje('error', 'Ambos datos son obligatorios');
-            return;
-        } else if (cantidad.value <= 0) {
-            interface.mostrarMensaje('error', 'La cantidad ingresada no es válida, intente de nuevo por favor');
-            return;
-        }
 
-        // muestra mensaje de éxito
-        interface.mostrarMensaje('sucess', 'Gasto agregado a la lista.')
+function expenses(event) {
+    event.preventDefault();
+
+    const campExpenses = form.querySelector('#gasto');
+    const campQuantity = form.querySelector('#cantidad');
+
+    // muestra alerta en caso de error o éxito
+    if(campExpenses.value === '' || campQuantity.value === '') {
+        interface.showAlert('error', 'Ambos campos son obligatorios.');
+        return;
+    } else if (campQuantity.value <= 0) {
+        interface.showAlert('error', 'Cantidad inválida, verifique e intente de nuevo por favor.');
+        return;
+    } else {
+        interface.showAlert('sucess', 'Gasto agregado a la lista.');
     }
+
+    //envia datos del formulario
+    data.getdata(campExpenses.value, campQuantity.value);
+
+    // reinicia el formulario
+    form.reset();
+
+    const list = data.expensesList;
+
+    // muestra lista en el html
+    interface.showList(list);
+
+    // Cálcula restante
+    data.restar(list);
+    interface.showRemain(data.remainder);
+
 }
